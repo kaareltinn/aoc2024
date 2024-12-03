bde6b89ff14efcebb83068e4a4565b422275383d
Kaarel Tinn <kaarel.tinn@every-pay.com>
2024-12-03 14:53:11 +0200

    Day2 part2 solution

--- day2/lib/day2_part2.ex
+++ day2/lib/day2_part2.ex
@@ -9,56 +9,45 @@ defmodule Day2Part2 do
 
   def run(input) do
     input
-    |> Enum.map(&check/1)
+    |> Enum.map(&check_with_level_removing/1)
     |> Enum.count(&(&1 == :safe))
   end
 
+  def check_with_level_removing(xs) do
+    cond do
+      check(xs) == :safe -> :safe
+      check(xs) == :unsafe ->
+        levels_removed =  (0..length(xs)-1) |> Enum.map(fn i -> List.delete_at(xs, i) end)
+        if Enum.any?(levels_removed, fn ys -> check(ys) == :safe end), do: :safe, else: :unsafe
+      true -> :unsafe
+    end
+  end
+
   def check(xs) do
-    check_helper(xs, nil, :unsafe, [], false)
+    check_helper(xs, nil, :unsafe)
   end
 
   # base
-  def check_helper([], _, res, _, _) do
+  def check_helper([], _, res) do
     res
   end
 
-  def check_helper([_x], _, res, _, _) do
+  def check_helper([_x], _, res) do
     res
   end
 
   # entry
-  def check_helper([x, y | rest], nil, res, visited, false) do
-    check_helper([x, y | rest], direction(x, y), res, visited, false)
+  def check_helper([x, y | rest], nil, res) do
+    check_helper([x, y | rest], direction(x, y), res)
   end
 
-  def check_helper([x, y | rest], prev_dir, _res, visited, changed) do
+  def check_helper([x, y | rest], prev_dir, _res) do
     diff = abs(x - y)
     cur_dir = direction(x, y)
-    visited_new = visited ++ [x]
-
-    dbg()
-
     cond do
-      diff < 4 ->
-        if diff == 0 and !changed do
-          check_helper(visited ++ [y] ++ rest, prev_dir, :unsafe, visited, true)
-        else
-          check_helper([y | rest], cur_dir, :safe, visited_new, changed)
-        end
-
-      diff == 0 ->
-        if changed do
-          check_helper([], cur_dir, :unsafe, visited_new, true)
-        else
-          check_helper(visited ++ [y] ++ rest, prev_dir, :unsafe, visited, true)
-        end
-
-      true ->
-        if changed do
-          check_helper([], cur_dir, :unsafe, visited_new, true)
-        else
-          check_helper(visited ++ [y] ++ rest, cur_dir, :unsafe, visited, true)
-        end
+      cur_dir != prev_dir -> check_helper([], cur_dir, :unsafe)
+      diff > 0 && diff < 4 -> check_helper([y | rest], cur_dir, :safe)
+      true -> check_helper([], cur_dir, :unsafe)
     end
   end
 
